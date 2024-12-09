c72b2feb0a32722dba2a5ca27cc9df8d8baf119f
LeMoiz <101976629+LeMoiz@users.noreply.github.com>
2024-12-09 12:00:52 +0100

    Update language_file_synchronizer.py
    Cleanup

--- src/language_file_synchronizer.py
+++ src/language_file_synchronizer.py
@@ -3,34 +3,32 @@ from googletrans import Translator
 
 def sync_languages(file_language_pairs, header_name, key, base_value=None):
     translator = Translator()
-    
-    if not base_value:
-        first_file, _ = file_language_pairs[0]
-        try:
-            with open(first_file, 'r', encoding='utf-8') as base_file:
-                base_value = json.load(base_file).get(header_name, {}).get(key)
-        except (json.JSONDecodeError, FileNotFoundError, IOError) as e:
-            print(f"Error processing {first_file}: {e}")
-            return
+    base_file, base_language = file_language_pairs[0]
+
+    if not base_language or not base_file:
+        print("Base language must be specified in the first tuple.")
+        return
 
     if not base_value:
-        print("No base value found or provided.")
-        return
+        try:
+            with open(base_file, 'r', encoding='utf-8') as bf:
+                base_value = json.load(bf).get(header_name, {}).get(key)
+        except:
+            print(f"Error reading {base_file}")
+            return
 
     for file_name, language in file_language_pairs:
         try:
             with open(file_name, 'r', encoding='utf-8') as file:
                 data = json.load(file)
-
             if header_name in data and isinstance(data[header_name], dict):
-                data[header_name][key] = translator.translate(base_value, dest=language).text if language else base_value
-
+                data[header_name][key] = (
+                    translator.translate(base_value, dest=language).text if language else base_value
+                )
             with open(file_name, 'w', encoding='utf-8') as file:
-                file.write(json.dumps(data, indent=2, ensure_ascii=False))
-
-            print(f"Updated {file_name} successfully.")
-        except (json.JSONDecodeError, FileNotFoundError, IOError) as e:
-            print(f"Error processing {file_name}: {e}")
+                json.dump(data, file, indent=2, ensure_ascii=False)
+        except:
+            print(f"Error processing {file_name}")
 
 # EXAMPLE USAGE
 file_language_pairs = [
